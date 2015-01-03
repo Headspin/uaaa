22aba90a5e442ef515fda78d8a0e89a071d61eb3
Headspin <simon.balazic@outlook.com>
2015-01-03 18:48:21 +0100

    Added ViewModel and Descriptor classes.

---
+++ Uaaa/Descriptor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uaaa {
+    /// <summary>
+    /// Append description to the model.
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public class Descriptor<TModel> : ViewModel<TModel> where TModel : Model {
+        /// <summary>
+        /// Item description.
+        /// </summary>
+        public string Description { get; protected set; }
+        /// <summary>
+        /// Creates new instance of descriptor class.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="description"></param>
+        public Descriptor(TModel item, string description)
+            : base(item) {
+            this.IsReadonly = true;
+            this.Description = description;
+        }
+    }
+}
---
+++ Uaaa/ViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uaaa {
+    /// <summary>
+    /// ViewModel base class that support property change notifications.
+    /// </summary>
+    public class ViewModel<TModel> : IViewModel, IModel, INotifyPropertyChanged where TModel : Model {
+        #region -=Properties/Fields=-
+        protected readonly PropertySetter Property = null;
+        private TModel _model = default(TModel);
+        public virtual TModel Model {
+            get { return _model; }
+            set {
+                if (Property.SetValue<TModel>(ref _model, value, allowChange: () => !this.IsReadonly || _model == default(TModel)))
+                    OnModelChanged();
+            }
+        }
+        /// <summary>
+        /// ViewModel is flagged as ReadOnly (model can only be set once).
+        /// </summary>
+        protected bool IsReadonly { get; set; }
+        #endregion
+        #region -=Constructors=-
+        public ViewModel() {
+            this.Property = new PropertySetter(this);
+        }
+        public ViewModel(TModel model)
+            : this() {
+            this.Model = model;
+        }
+        #endregion
+        #region -=Protected methods=-
+        /// <summary>
+        /// Triggers optional actions after model value changed.
+        /// </summary>
+        protected virtual void OnModelChanged() { }
+        #endregion
+        #region -=IModel members=-
+        public event PropertyChangedEventHandler PropertyChanged;
+        void IModel.RaisePropertyChanged(string propertyName) {
+            RaisePropertyChanged(propertyName);
+        }
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null) {
+            if (string.IsNullOrEmpty(propertyName)) return;
+            if (PropertyChanged != null)
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+        #region -=IViewModel members=-
+        object IViewModel.GetModel() {
+            return _model;
+        }
+        void IViewModel.SetModel(object model) {
+            this.Model = model as TModel;
+        }
+        #endregion
+        #region -=Static members=-
+        /// <summary>
+        /// Returns model.
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
+        public static implicit operator TModel(ViewModel<TModel> viewModel) {
+            return viewModel.Model;
+        }
+        #endregion
+    }
+}
