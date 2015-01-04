c8220a43235abb9f27ed1c108548964fc267c930
Headspin <simon.balazic@outlook.com>
2015-01-04 12:42:16 +0100

    Descriptor not subclass of ViewModel any more.

---
+++ Uaaa/Components/Descriptor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uaaa {
+    /// <summary>
+    /// Append description to the value.
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    public class Descriptor<TValue> : IViewModel {
+        public TValue Value { get; private set; }
+        /// <summary>
+        /// Item description.
+        /// </summary>
+        public string Description { get; protected set; }
+        /// <summary>
+        /// Creates new instance of descriptor class.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="description"></param>
+        public Descriptor(TValue value, string description) {
+            this.Value = value;
+            this.Description = description;
+        }
+        #region -=IViewModel members=-
+        object IViewModel.GetModel() {
+            return this.Value;
+        }
+        void IViewModel.SetModel(object model) {
+            return; //NOTE: Descriptor value is readonly.
+        }
+        #endregion
+        #region -=Static members=-
+        /// <summary>
+        /// Returns model.
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
+        public static implicit operator TValue(Descriptor<TValue> descriptor) {
+            return descriptor.Value;
+        }
+        #endregion
+    }
+}
