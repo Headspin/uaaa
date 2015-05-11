b45af09d5b3c74cee30f046fbb1096aa888d7b88
Headspin <simon.balazic@outlook.com>
2015-05-11 21:10:16 +0200

    Added test for multiple rules per property.

--- UaaaNUnit/BusinessRulesCheckerTest.cs
+++ UaaaNUnit/BusinessRulesCheckerTest.cs
@@ -51,7 +51,62 @@ namespace UaaaNUnit {
 
 			testModel.Label = "Label1";
 			result = checker.IsValid (testModel);
-			errors = checker.GetErrorsCollection("");
+			errors = checker.GetErrorsCollection ("");
+			Assert.IsTrue (result, "Checker result should be valid.");
+			Assert.IsFalse (checker.HasErrors, "No errors expected.");
+			Assert.AreEqual (0, errors.Count, "Invalid errors count.");
+
+			// check 2nd property rules.
+			testModel.Value = 1;
+			result = checker.IsValid (testModel);
+			errors = checker.GetErrorsCollection ("");
+			Assert.IsFalse (result, "Checker result should not be valid.");
+			Assert.IsTrue (checker.HasErrors, "Errors expected.");
+			Assert.AreEqual (1, errors.Count, "Invalid errors count.");
+			Assert.AreEqual ("Error2", errors.First (), "Invalid error");
+
+			testModel.Value = 10;
+			result = checker.IsValid (testModel);
+			errors = checker.GetErrorsCollection ("");
+			Assert.IsTrue (result, "Checker result should be valid.");
+			Assert.IsFalse (checker.HasErrors, "No errors expected.");
+			Assert.AreEqual (0, errors.Count, "Invalid errors count.");
+		}
+
+		[Test ()]
+		public void BusinessRulesChecker_MultipleRules_Per_Property () {
+			TestModel testModel = new TestModel () { Label = "Label1", Value = 10 };
+			BusinessRulesChecker checker = new BusinessRulesChecker ();
+			checker.Add (new GenericRule<TestModel> (model => model.Label.EndsWith("1"), "Error1.1"),
+				"Label");
+			checker.Add (new GenericRule<TestModel> (model => model.Label.StartsWith("Label"), "Error1.2"), 
+				"Label");
+			checker.Add (new GenericRule<TestModel> (model => model.Value == 10, "Error2"), 
+				"Value");
+
+			Assert.IsFalse (checker.HasErrors, "No errors expected.");
+			bool result = checker.IsValid (testModel);
+			Assert.IsTrue (result, "Checker result should be valid.");
+
+			testModel.Label = "Label2";
+			result = checker.IsValid (testModel);
+			Items<string> errors = checker.GetErrorsCollection ("");
+			Assert.IsFalse (result, "Checker result should not be valid.");
+			Assert.IsTrue (checker.HasErrors, "Errors expected.");
+			Assert.AreEqual (1, errors.Count, "One error expected.");
+			Assert.AreEqual ("Error1.1", errors.First (), "Invalid error.");
+
+			testModel.Label = "1";
+			result = checker.IsValid (testModel);
+			errors = checker.GetErrorsCollection ("");
+			Assert.IsFalse (result, "Checker result should not be valid.");
+			Assert.IsTrue (checker.HasErrors, "Errors expected.");
+			Assert.AreEqual (1, errors.Count, "One error expected.");
+			Assert.AreEqual ("Error1.2", errors.First (), "Invalid error.");
+
+			testModel.Label = "Label1";
+			result = checker.IsValid (testModel);
+			errors = checker.GetErrorsCollection ("");
 			Assert.IsTrue (result, "Checker result should be valid.");
 			Assert.IsFalse (checker.HasErrors, "No errors expected.");
 			Assert.AreEqual (0, errors.Count, "Invalid errors count.");
