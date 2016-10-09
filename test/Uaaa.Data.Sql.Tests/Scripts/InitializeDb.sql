50c5a305b44644d8747cbde45a787b4db13bad0d
Headspin <simon.balazic@outlook.com>
2016-10-09 09:21:19 +0200

    Fixed initialization script.

--- test/Uaaa.Data.Sql.Tests/Scripts/InitializeDb.sql
+++ test/Uaaa.Data.Sql.Tests/Scripts/InitializeDb.sql
@@ -1,1 +1,9 @@
-﻿delete from People;
+﻿-- create people table
+create table People(
+	Id int identity(1,1) not null primary key,
+	Name nvarchar(50) not null,
+	Surname nvarchar(50) not null,
+	Age int null,
+	CreateDateTimeUtc datetime not null,
+	ChangedDateTimeUtc datetime null
+);
