fec7f8825b591d807931e21612866807932e6f05
Headspin <simon.balazic@outlook.com>
2016-10-26 10:15:35 +0200

    Added RecordNotFoundException

---
+++ src/Uaaa.Core/Data/RecordNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uaaa.Core.Data
+{
+    /// <summary>
+    /// Exception is raised when requested record was not found.
+    /// </summary>
+    public class RecordNotFoundException:Exception
+    {
+        public int? Key { get; private set; }
+        public RecordNotFoundException(string message) : base(message) { }
+
+        public RecordNotFoundException(string message, int key) : this(message)
+        {
+            Key = key;
+        }
+    }
+}
