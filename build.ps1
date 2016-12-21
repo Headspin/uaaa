6cc4d42117283c23d19fb7255dc5cdc3e2dab16d
Headspin <Simon Balazic@metrel.si>
2016-12-21 11:27:53 +0100

    added rebuild, test and pack vscode tasks.

--- build.ps1
+++ build.ps1
@@ -1,2 +1,12 @@
-dotnet build ./**/project.json
+Param(
+    [string]$option
+)
+if ($option){
+    Write-Host "Rebuilding projects..." -ForegroundColor Cyan
+    dotnet build ./**/project.json $option
+}
+else{
+    Write-Host "Building projects..." -ForegroundColor Cyan
+    dotnet build ./**/project.json
+}
 Write-Host "Finished."
